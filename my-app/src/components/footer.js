993bbbaf7c6bb52305989796eba0af7cc7fa404f
MariFromNZ <mariia.fotina@gmail.com>
2024-09-19 15:25:41 +1200

    added img for footer

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -1,13 +1,14 @@
 // (Mariia) created Footer component with image that linking to uiatetaiao.nz, address and link to terms of use
 //-------------------------------------------------------------------------------------------------------------//
 // should change css for footer_content_address after adding bootstrap 
+import tuiTeTaiaoLogo from '../images/tuiTeTaiaoLogo.svg';
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer_content">
             <a href="https://www.tuiatetaiao.nz/" rel="noreferrer" target="_blank">
-                <img  src="" alt="logo of tuiatetaiao website"/>
+                <img  src={tuiTeTaiaoLogo} alt="logo logo of tuiatetaiao website"/>
             </a>
             <div className="footer_content_address">
                 <p>Level 3, 5 Willeston Street</p>
