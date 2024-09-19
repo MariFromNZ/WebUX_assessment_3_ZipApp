17921d7a5be2aaf48cd83149f2442703477a4ccc
MariFromNZ <mariia.fotina@gmail.com>
2024-09-20 02:21:39 +1200

    added about, recent posts and saved posts

--- my-app/src/pages/ProfilePage.js
+++ my-app/src/pages/ProfilePage.js
@@ -21,10 +21,48 @@ function ProfilePage() {
               </Col>
               <Col xs={6} className='profile_content_details_name'>
                <h2>Name</h2>
-                <p>location</p>
+                <p >location</p>
                 <p>5 followers</p>
                 <p>13 following</p>
              </Col>
+           </Row>
+           <Row className='profile_content_about row-margin-bottom'>
+              <Col xs={12}>
+                <h4>About me</h4>
+                <p>I’m an outdoor enthusiast who loves exploring nature and snapping photos of wildlife. Hiking, birdwatching, and capturing the beauty of plants and animals is my passion. </p>
+              </Col>
+           </Row>
+
+           <Row className='profile_content_recent row-margin-bottom'>
+              <h4>My recent posts</h4>
+              <Col xs={6}>
+                <h6>post 1</h6>
+              </Col>
+              <Col xs={6}>
+                <h6>post 2</h6>
+              </Col>
+              <Col xs={6}>
+                <h6>post 3</h6>
+              </Col>
+              <Col xs={6}>
+                <h6>post 4</h6>
+              </Col>
+           </Row>
+
+           <Row className='profile_content_saved row-margin-bottom'>
+              <h4>My saved posts</h4>
+              <Col xs={6}>
+                <h6>post 1</h6>
+              </Col>
+              <Col xs={6}>
+                <h6>post 2</h6>
+              </Col>
+              <Col xs={6}>
+                <h6>post 3</h6>
+              </Col>
+              <Col xs={6}>
+                <h6>post 4</h6>
+              </Col>
            </Row>
           </Container>
         </div>
