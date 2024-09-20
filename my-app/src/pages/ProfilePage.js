cf028e133d05e59752e2b558160323ea8b613613
MariFromNZ <mariia.fotina@gmail.com>
2024-09-20 17:00:27 +1200

    added images to cards

--- my-app/src/pages/ProfilePage.js
+++ my-app/src/pages/ProfilePage.js
@@ -2,6 +2,12 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/footer'
 import profileIcon from '../images/pukeko.jpg';
+import weka from '../images/weka.jpg';
+import spottedLizard from '../images/spottedLizard.jpg';
+import greenLizard from '../images/greenLizard.jpg';
+import kea from '../images/kea.jpg';
+import furSeal from '../images/furSeal.jpg';
+import shortTailedBat from '../images/shortTailedBat.jpg';
 import bookmarkIcon from '../images/bookmarkIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -46,9 +52,9 @@ function ProfilePage() {
               <h4>My recent posts</h4>
               <Col xs={6}>
                 <Card className='card-position'>
-                  <Card.Img variant="top" src={profileIcon} alt="user avatar" />
+                  <Card.Img variant="top" src={spottedLizard} alt="spotted lizard on the stone" />
                   <Card.Body className='d-flex justify-content-between align-items-center'>
-                    <Card.Title className='card-transparent-bg card-title-overlay'>Pukeko</Card.Title>
+                    <Card.Title className='card-transparent-bg card-title-overlay'>Spotted lizard</Card.Title>
                     <div className='card_icons d-flex'>
                       <img  src={bookmarkIcon} alt="bookmark" className='bookmark icon'/>
                       <img  src={shareIcon} alt="share" className='share icon'/>
@@ -59,9 +65,9 @@ function ProfilePage() {
 
               <Col xs={6}>
                 <Card className='card-position'>
-                  <Card.Img variant="top" src={profileIcon} alt="user avatar" />
+                  <Card.Img variant="top" src={weka} alt="bird weka"/>
                   <Card.Body className='d-flex justify-content-between align-items-center'>
-                    <Card.Title className='card-transparent-bg card-title-overlay'>Pukeko</Card.Title>
+                    <Card.Title className='card-transparent-bg card-title-overlay'>Weka</Card.Title>
                     <div className='card_icons d-flex'>
                       <img  src={bookmarkIcon} alt="bookmark" className='bookmark icon'/>
                       <img  src={shareIcon} alt="share" className='share icon'/>
@@ -72,9 +78,9 @@ function ProfilePage() {
 
               <Col xs={6}>
                 <Card className='card-position'>
-                  <Card.Img variant="top" src={profileIcon} alt="user avatar" />
+                  <Card.Img variant="top" src={kea} alt="user avatar" />
                   <Card.Body className='d-flex justify-content-between align-items-center'>
-                    <Card.Title className='card-transparent-bg card-title-overlay'>Pukeko</Card.Title>
+                    <Card.Title className='card-transparent-bg card-title-overlay'>Kea</Card.Title>
                     <div className='card_icons d-flex'>
                       <img  src={bookmarkIcon} alt="bookmark" className='bookmark icon'/>
                       <img  src={shareIcon} alt="share" className='share icon'/>
@@ -102,9 +108,22 @@ function ProfilePage() {
 
               <Col xs={6}>
                 <Card className='card-position'>
-                  <Card.Img variant="top" src={profileIcon} alt="user avatar" />
+                  <Card.Img variant="top" src={furSeal} alt="fur seal"/>
                   <Card.Body className='d-flex justify-content-between align-items-center'>
-                    <Card.Title className='card-transparent-bg card-title-overlay'>Pukeko</Card.Title>
+                    <Card.Title className='card-transparent-bg card-title-overlay'>Fue seal</Card.Title>
+                    <div className='card_icons d-flex'>
+                      <img  src={bookmarkIcon} alt="bookmark" className='bookmark icon'/>
+                      <img  src={shareIcon} alt="share" className='share icon'/>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={6}>
+                <Card className='card-position'>
+                  <Card.Img variant="top" src={shortTailedBat} alt="shorttailed bat" />
+                  <Card.Body className='d-flex justify-content-between align-items-center'>
+                    <Card.Title className='card-transparent-bg card-title-overlay'>Shorttailed bat</Card.Title>
                     <div className='card_icons d-flex'>
                       <img  src={bookmarkIcon} alt="bookmark" className='bookmark icon'/>
                       <img  src={shareIcon} alt="share" className='share icon'/>
@@ -128,22 +147,9 @@ function ProfilePage() {
 
               <Col xs={6}>
                 <Card className='card-position'>
-                  <Card.Img variant="top" src={profileIcon} alt="user avatar" />
+                  <Card.Img variant="top" src={greenLizard} alt="green Lizard" />
                   <Card.Body className='d-flex justify-content-between align-items-center'>
-                    <Card.Title className='card-transparent-bg card-title-overlay'>Pukeko</Card.Title>
-                    <div className='card_icons d-flex'>
-                      <img  src={bookmarkIcon} alt="bookmark" className='bookmark icon'/>
-                      <img  src={shareIcon} alt="share" className='share icon'/>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-
-              <Col xs={6}>
-                <Card className='card-position'>
-                  <Card.Img variant="top" src={profileIcon} alt="user avatar" />
-                  <Card.Body className='d-flex justify-content-between align-items-center'>
-                    <Card.Title className='card-transparent-bg card-title-overlay'>Pukeko</Card.Title>
+                    <Card.Title className='card-transparent-bg card-title-overlay'>Green lizard</Card.Title>
                     <div className='card_icons d-flex'>
                       <img  src={bookmarkIcon} alt="bookmark" className='bookmark icon'/>
                       <img  src={shareIcon} alt="share" className='share icon'/>
