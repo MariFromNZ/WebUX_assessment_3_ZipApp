741b615b807a25493dc2280dd5b8ceb69fc724b4
Gaston Junca <g.albertojunca@gmail.com>
2024-09-25 16:51:17 +1200

    Gaston/EditPostPage/ finished the editing post page

--- my-app/src/pages/EditPostPage.js
+++ my-app/src/pages/EditPostPage.js
@@ -1,11 +1,33 @@
 // (Gaston) edit posts page
 import React from 'react'
 import '../pages/EditPostPageStyle.css'
+import addPhotoIcon from "../images/addPhotoIcon.svg"
 
 function EditPostPage() {
   return (
-    <div className='title'>
-        <h1>Edit Post</h1>
+    <div className='edit-post-container'>
+      <div className='title'>
+          <h1>Edit Post</h1>
+      </div>
+      <div className='post-photo'>
+        <a href=''> <img className='add-photo-icon' src={addPhotoIcon}></img> </a>
+      </div>
+      <div className='post-species'>
+        <label for="username">Name of species</label>
+        <input type="text" id="species" placeholder='Fantail'></input>
+      </div>
+      <div className='post-description'>
+        <label for="username">Description</label>
+        <input type="text" id="description" placeholder='Found the guy while...'></input>
+      </div>
+      <div className='post-location'>
+        <label for="username">Location</label>
+        <input type="text" id="location" placeholder='Location'></input>
+      </div>
+      <div className='post-btn'>
+        <a href=''><button className='btn-discard'>Discard</button></a>
+        <a href='profile'><button className='btn-post'>Post</button></a>
+      </div>
     </div>
   )
 }
